21281b5d11eb56ef52299fdea7439340965918e0
DraxCraft132 <57847505+DraxCraft132@users.noreply.github.com>
2023-02-02 23:22:57 -0300

    Giovani
    Hecho para tener centrado el proyecto

---
+++ C++/Ejercicios/Basico/operacionaritmetica.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+    int main(){
+        float n1,n2,suma,resta,multi,div;
+        cout<<"Digita un numero: ";
+        cin>> n1;
+        cout<<"Digita otro numero: ";
+        cin>> n2;
+        n1+n2;
+        n1-n2;
+        multi=n1*n2;
+        div=n1/n2;
+        cout<<"El resultado de la Suma: "<<suma<<"\n";
+        cout<<"El resultado de la Resta: "<<resta<<"\n";
+        cout<<"El resultado de la Multiplicacion:"<<multi<<"\n";
+        cout<<"El resultado de la Divicion: "<<div<<"\n";
+
+    system("pause");
+    return 0;
+}
